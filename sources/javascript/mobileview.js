f205e2f5349cc25a1fcb1f6b37e65f10bec71274
Jay <109523337+jayverrijt@users.noreply.github.com>
2022-10-20 10:04:02 +0200

    [V2 Release]
    - Toch nog geen Mobile view
    - Sidebar (met social media buttons) is nu zichtbaar op elke pagina

--- sources/javascript/mobileview.js
+++
@@ -1,8 +0,0 @@
-function SwitchToMobile () {
-    if (
-        navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)
-    ) { window.location.href = "mobile.html";} else 
-    { console.log("Agent is not an Phone!");}
-}
-
-SwitchToMobile();
